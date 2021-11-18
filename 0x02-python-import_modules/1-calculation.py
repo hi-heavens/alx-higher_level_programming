a5f5861a709ee6c78c82e47057bc4013abe893fc
Kehinde Adedokun <ken_adex@yahoo.com>
2021-11-18 23:39:02 +0100

    A program that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{2:d} + {1:d} = {:d}".format(add(a, b), b, a))
-    print("{2:d} - {1:d} = {:d}".format(sub(a, b), b, a))
-    print("{2:d} * {1:d} = {:d}".format(mul(a, b), b, a))
-    print("{2:d} / {1:d} = {:d}".format(div(a, b), b, a))
+    print("{2:d} + {1:d} = {0:d}".format(add(a, b), b, a))
+    print("{2:d} - {1:d} = {0:d}".format(sub(a, b), b, a))
+    print("{2:d} * {1:d} = {0:d}".format(mul(a, b), b, a))
+    print("{2:d} / {1:d} = {0:d}".format(div(a, b), b, a))
