253a6da5e4381081a2e0173f7691f222300b06ab
hi-heavens <ken_adex@yahoo.com>
2021-12-08 22:32:16 +0100

    A class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-The size of a square is crucial for a square, many things depend of it
-(area computation, etc.), so you, as class builder, must control the
-type and value of this attribute. One way to have the control is to
-keep it privately.
-You will see in next tasks how to get, update and validate the size value
-"""
+"""We are going to define the class here"""
 
 
 class Square:
     """A class Square that defines a square and initialize it"""
+
+
     def __init__(self, size):
-      """Initialization of the size field"""
+        """Initialization of the size field"""
+
         self.__size = size
