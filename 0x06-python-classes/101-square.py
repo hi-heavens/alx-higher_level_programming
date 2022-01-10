ab2ee623116423fd4072af7d31f583fdcfd429c7
Kehinde Adedokun <ken_adex@yahoo.com>
2022-01-10 23:07:45 +0100

    A class Square that defines a square by: (based on 6-square.py)

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0, position=(0, 0)):
+        """This is to initialize a new square to an object.
+
+        Args:
+            size: The size of the square object
+            position: The position of the square object
+        """
         self.size = size
         self.position = position
    
     @property
     def size(self):
+        """This is to retrieve the value of the size variable"""
         return self.__size
 
     @size.setter
@@ -19,6 +26,7 @@ class Square:
     
     @property
     def position(self):
+        """This is to retrieve the value of the position variable"""
         return self.__position
 
     @position.setter
@@ -32,10 +40,26 @@ class Square:
         self.__position = position
 
     def area(self):
+        """This method returns the area of the size variable"""
         return self.__size ** 2
 
+    def my_print(self):
+        """This method prints the square using the # symbol"""
+        if self.__size == 0:
+            print()
+        else:
+            print('\n' * self.__position[1], end='')
+            for num in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
 
-my_square = Square(5, (0, 0))
-print(my_square)
-
-print("--")
+    def __str__(self):
+        """This method returns the same behavior as my_print()"""
+        if self.__size == 0:
+            print()
+        else:
+            print('\n' * self.__position[1], end='')
+            for num in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
+        return ""
