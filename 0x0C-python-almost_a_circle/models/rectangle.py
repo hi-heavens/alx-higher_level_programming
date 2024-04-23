07e9582e72069ea27510e3bdfff73c0350f238c6
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-04-23 16:52:32 +0100

    print in stdout the Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,7 +71,11 @@ class Rectangle(Base):
 
     def display(self):
         """ Prints in stdout the Rectangle instance with the character # """
+        for noline in range(self.__y):
+            print("")
         for i in range(self.__height):
+            for space in range(self.__x):
+                print(" ", end='')
             for j in range(self.__width):
                 print("#", end="")
             print("")
