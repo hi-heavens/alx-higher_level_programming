cf703cf6093670bbcf1ccc521a71e789174e2d58
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-04-23 16:31:49 +0100

    overriding the __str__ method in the Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        """ Update the class Rectangle by overriding the __str__ method """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
