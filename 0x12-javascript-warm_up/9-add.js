c7a771a0766ecfb3ef7e12ca0a7eb6661af8fdfc
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-06-21 21:19:55 +0100

    Fix the semistandard error with function parens

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -12,7 +12,7 @@ Write a script that prints the addition of 2 integers
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   console.log(a + b);
 }
 
