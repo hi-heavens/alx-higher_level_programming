4acb3468aad2701274634add20507d51793069b4
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-06-21 21:29:23 +0100

    removed the unused variables

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -6,9 +6,6 @@ Write a function that returns the addition of 2 integers.
 - The function must be visible from outside
 - The name of the function must be add
 */
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
 function add (a, b) {
   return a + b;
 }
